a262a5b9449a3628d8372020a9cad65310778c54
namelesslogger <johnshanie@gmail.com>
2020-11-15 19:26:46 +0000

    [update] more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
-use std::io::prelude::*;
-use std::thread;
-use std::time::Duration;
-
-
-
-/// A simple example demonstrating how to handle user input. This is
-/// a bit out of the scope of the library as it does not provide any
-/// input handling out of the box. However, it may helps some to get
-/// started.
-///
-/// This is a very simple example:
-///   * A input box always focused. Every character you type is registered
-///   here
-///   * Pressing Backspace erases a character
-///   * Pressing Enter pushes the current input in the history of previous
-///   messages
-
 #[allow(dead_code)]
 mod util;
 
-use crate::util::event::{Event, Events};
-use std::{error::Error, io};
+use std::{
+    error::Error, 
+    io::{stdout, stdin, Write}};
 use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
 use tui::{
     backend::TermionBackend,
@@ -31,217 +14,68 @@ use tui::{
     Terminal,
 };
 use chrono::{DateTime, Local};
-
+use std::sync::mpsc::{sync_channel, channel};
 use unicode_width::UnicodeWidthStr;
+use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
 
-enum InputMode {
-    Normal,
-    Editing,
+fn main() {
+    run();
 }
 
-struct Message {
-    date: DateTime<Local>,
-    user: String,
-    message: String
+fn run() {
+    thread::spawn(||{
+        server();
+    });
+    client();
 }
 
-/// App holds the state of the application
-struct App {
-    /// Current value of the input box
-    input: String,
-    /// Current input mode
-    input_mode: InputMode,
-    /// History of recorded messages
-    messages: Vec<Message>,
-}
+fn client() {
+    let context = zmq::Context::new();
 
-impl Default for App {
-    fn default() -> App {
-        App {
-            input: String::new(),
-            input_mode: InputMode::Normal,
-            messages: Vec::new(),
-        }
-    }
-}
+    let sender = context.socket(zmq::REQ).unwrap();
+    let reciever = context.socket(zmq::REP).unwrap();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // Spawn the client and server
-    // spawn()
-    // Terminal initialization
-    let stdout = io::stdout().into_raw_mode()?;
-    let stdout = MouseTerminal::from(stdout);
-    let stdout = AlternateScreen::from(stdout);
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    assert!(sender.connect("tcp://localhost:5555").is_ok());
+    assert!(reciever.connect("tcp://localhost:5556").is_ok());
+    
+    // waits for input
+    thread::spawn(move || loop { 
+        let mut s=String::new();
+        let _=stdout().flush();
+        stdin().read_line(&mut s).expect("What");   
+        
+        sender.send(zmq::Message::from(&s), 0).unwrap();
+        sender.recv(&mut zmq::Message::new(), 0).unwrap();
+    });
 
-    // Setup event handlers
-    let mut events = Events::new();
-
-    // Create default app state
-    let mut app = App::default();
-
+    let mut msg = zmq::Message::new();
     loop {
-        // Draw UI
-        terminal.draw(|f| {
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(2)
-                .constraints(
-                    [
-                        Constraint::Length(1),
-                        Constraint::Min(1),
-                        Constraint::Length(3),
-                    ]
-                    .as_ref(),
-                )
-                .split(f.size());
-
-            let (msg, style) = match app.input_mode {
-                InputMode::Normal => (
-                    vec![
-                        Span::raw("Press "),
-                        Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw(" to exit, "),
-                        Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw(" to start editing."),
-                    ],
-                    Style::default().add_modifier(Modifier::RAPID_BLINK),
-                ),
-                InputMode::Editing => (
-                    vec![
-                        Span::raw("Press "),
-                        Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw(" to stop editing, "),
-                        Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw(" to record the message"),
-                    ],
-                    Style::default(),
-                ),
-            };
-            let mut text = Text::from(Spans::from(msg));
-            text.patch_style(style);
-            let help_message = Paragraph::new(text);
-            f.render_widget(help_message, chunks[0]);
-
-            let input = Paragraph::new(app.input.as_ref())
-                .style(match app.input_mode {
-                    InputMode::Normal => Style::default(),
-                    InputMode::Editing => Style::default().fg(Color::Yellow),
-                })
-                .block(Block::default().borders(Borders::ALL).title("Message"));
-            f.render_widget(input, chunks[2]);
-            match app.input_mode {
-                InputMode::Normal =>
-                    // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-                    {}
-
-                InputMode::Editing => {
-                    // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
-                    f.set_cursor(
-                        // Put cursor past the end of the input text
-                        chunks[2].x + app.input.width() as u16 + 1,
-                        // Move one line down, from the border to the input line
-                        chunks[2].y + 1,
-                    )
-                }
-            }
-
-            let messages: Vec<ListItem> = app
-                .messages
-                .iter()
-                // .enumerate()
-                .map(|(m)| {
-                    let content = vec![Spans::from(Span::raw(format!("{}: {}: {}", m.date.to_rfc3339(), m.user, m.message)))];
-                    ListItem::new(content)
-                })
-                .collect();
-            let messages =
-                List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-            f.render_widget(messages, chunks[1]);
-        })?;
-
-        // Handle input
-        if let Event::Input(input) = events.next()? {
-            match app.input_mode {
-                InputMode::Normal => match input {
-                    Key::Char('e') => {
-                        app.input_mode = InputMode::Editing;
-                        events.disable_exit_key();
-                    }
-                    Key::Char('q') => {
-                        break;
-                    }
-                    _ => {}
-                },
-                InputMode::Editing => match input {
-                    Key::Char('\n') => {
-                        let new_message = Message{
-                            date: Local::now(),
-                            user: whoami::username(),
-                            message: app.input.drain(..).collect()
-                        };
-                        app.messages.push(new_message);
-                    }
-                    Key::Char(c) => {
-                        app.input.push(c);
-                    }
-                    Key::Backspace => {
-                        app.input.pop();
-                    }
-                    Key::Esc => {
-                        app.input_mode = InputMode::Normal;
-                        events.enable_exit_key();
-                    }
-                    _ => {}
-                },
-            }
-        }
+        reciever.recv(&mut msg, 0).unwrap();
+        let message_txt = msg.as_str().unwrap();
+        reciever.send("recieved", 0).unwrap();
+        println!("{}", message_txt);
     }
-    Ok(())
 }
 
+fn server() {
+    let context = zmq::Context::new();
 
+    let reciever = context.socket(zmq::REP).unwrap();
+    let responder = context.socket(zmq::REQ).unwrap();
 
-fn spawn() {
-    println!("Connecting to hello world server...\n");
+    assert!(reciever.bind("tcp://*:5555").is_ok());
+    assert!(responder.bind("tcp://*:5556").is_ok());
 
-    // thread::spawn(|| {
-    //     server();
-    // });
-    // client();
+    let mut msg = zmq::Message::new();
+    loop {
+        reciever.recv(&mut msg, 0).unwrap();
+        reciever.send("recieved", 0).unwrap();
+        
+        let message_txt = msg.as_str().unwrap();
+        
+        responder.send(zmq::Message::from(message_txt), 0).unwrap();
+        responder.recv(&mut msg, 0).unwrap();
+    }
 }
-
-
-// fn client() {
-//     let context = zmq::Context::new();
-//     let requester = context.socket(zmq::REQ).unwrap();
-
-//     assert!(requester.connect("tcp://localhost:5555").is_ok());
-
-//     let mut msg = zmq::Message::new();
-
-//     for request_nbr in 0..10 {
-//         println!("Sending Hello {}...", request_nbr);
-//         requester.send("Hello", 0).unwrap();
-
-//         requester.recv(&mut msg, 0).unwrap();
-//         println!("Received World {}: {}", msg.as_str().unwrap(), request_nbr);
-
-//     }
-// }
-
-// fn server() {
-//     let context = zmq::Context::new();
-//     let responder = context.socket(zmq::REP).unwrap();
-
-//     assert!(responder.bind("tcp://*:5555").is_ok());
-
-//     let mut msg = zmq::Message::new();
-//     loop {
-//         responder.recv(&mut msg, 0).unwrap();
-//         println!("Received {}", msg.as_str().unwrap());
-//         thread::sleep(Duration::from_millis(1000));
-//         responder.send("World", 0).unwrap();
-//     }
-// }
